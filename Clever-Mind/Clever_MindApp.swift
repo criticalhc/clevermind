7d26aa637efe1bef929415802fbfe9c4d10a7821
Heydon Costello <heydonc@icloud.com>
2022-11-03 19:14:06 +0000

    Initial Commit

---
+++ Clever-Mind/Clever_MindApp.swift
@@ -0,0 +1,17 @@
+//
+//  Clever_MindApp.swift
+//  Clever-Mind
+//
+//  Created by Heydon Costello on 03/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct Clever_MindApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
